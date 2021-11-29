bac39d4913a7062686aebbc9feb1866545547baa
Mohamed A. Ibrahim <rageali12@gmail.com>
2021-11-29 11:39:04 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-//VANILLA JS
-var increaseBtn = document.querySelector('.fa-plus-circle');
-var decreaseBtn = document.querySelector('.fa-minus-circle');
-var hamburgerIcon = document.querySelector('#hamburgerIcon')
+var hamburgerIcon = document.querySelector('#hamburgerIcon');
 
 hamburgerIcon.addEventListener('click', ()=>{
   let menuItem = document.querySelector('#menu-item');
   menuItem.style.display="block";
 });
+//promp for delivery or pickup
+$('.deliveryLocationPrompt').change(function(){
+  let deliveryClicked = $(this).children('option:selected').data('1')
+  let location = prompt("what is the Location of your Delivery?") 
+  alert("your order will be delivered to your location")
+})
 //constructor
 function Pizza (crust, topping, size){
   this.crustPizza = crust;
@@ -15,14 +18,18 @@ function Pizza (crust, topping, size){
 }
 
 
+
 $().ready(function(){
   
   $('#checked').submit( function(e){
+    e.preventDefault();
     // data of selected options
     var sizeSelected = $('#PizzaSizeSelect').find(":selected").text();
     var crustSelected = $('#PizzaCrustSelect').find(":selected").text();
     var toppingSelected = $('#PizzaToppingSelect').find(":selected").text();
-    var payAmounth = $('.payAmount').text();
+    var deliverySelected = $('#deliverySelect').find(":selected").text();
+
+   
    var obj = {
       "Large":1000,
       "Small":600,
@@ -35,16 +42,29 @@ $().ready(function(){
       "mushrooms":250,
       "Crispy":260,
       "Stuffed":270,
-     "Gluten-free":280
+     "Gluten-free":280,
+     "Delivery":100,
+     "Pick-up":0
    }
-   var totalCost = (obj[sizeSelected] + obj[crustSelected] + obj[toppingSelected]);
-    alert(totalCost)
+   var totalCost = (obj[sizeSelected] + obj[crustSelected] + obj[toppingSelected]) + obj[deliverySelected] ;
+
+  
+  $('#summ').html('View Summary')
+  $('.payAmount').html(`KES ${totalCost}`)
+  $('.pizzaToppingofChoice').html(`Pizza Topping Selected cost:KES  ${obj[toppingSelected]}`);
+  $('.pizzaSizeofChoice').html(`Pizza Size Selected cost:KES  ${obj[sizeSelected]}`);
+  $('.pizzaCrustofChoice').html(`Pizza Crust Selected cost: KES ${obj[crustSelected]}`);
+  $('.pizzaDeliveryOrPickupChoice').html(`delivery is :KES  ${obj[deliverySelected]}`);
   })
+ 
+
+
+  
+
  
 })
 
 
-// mobile view menu
 
 
 
